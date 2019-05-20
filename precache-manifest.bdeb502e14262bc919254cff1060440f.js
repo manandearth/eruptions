fef41b6fad845cba459214d159b1fd0c170d58c0
manandearth <adamgefen@posteo.net>
2019-05-20 21:24:31 +0200

    Updates

---
+++ precache-manifest.bdeb502e14262bc919254cff1060440f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f140cd0f5c5de4609439b09be4116c",
+    "url": "/eruptions/index.html"
+  },
+  {
+    "revision": "a0eee2b2c029a7ece28a",
+    "url": "/eruptions/static/css/main.90d2eb96.chunk.css"
+  },
+  {
+    "revision": "e27cb9e45a564e54d6e3",
+    "url": "/eruptions/static/js/2.1dc0e067.chunk.js"
+  },
+  {
+    "revision": "a0eee2b2c029a7ece28a",
+    "url": "/eruptions/static/js/main.de1eb204.chunk.js"
+  },
+  {
+    "revision": "fe9ffed8716bcef62930",
+    "url": "/eruptions/static/js/runtime~main.56c3a6cd.js"
+  }
+]);
